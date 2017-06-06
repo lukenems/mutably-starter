436e7f78dfe385a509474f9aac63a5434ef37f71
smithrm941 <smithrm941@gmail.com>
2017-06-06 11:06:38 -0700

    added initial AJAX request for pokemon data

--- public/script.js
+++ public/script.js
@@ -1,18 +1,26 @@
 console.log("Sanity Check: JS is working!");
 
-$(document).ready(function(){
 
+// $(document).ready(function(){
   var ourRequest = new XMLHttpRequest();
+  var pokemonContainer = document.getElementById("pokedex")
 
   ourRequest.open('GET', 'https://mutably.herokuapp.com/pokemon');
 
   ourRequest.onload = function() {
     var pokedexData = JSON.parse(ourRequest.responseText);
-    console.log(pokedexData);
-    // renderHTML(pokedexData);
+    // console.log(pokedexData);
+    // pokemonContainer.insertAdjacentHTML('beforeend', pokedexData.pokemon[0].name);
+    renderHTML(pokedexData);
   };
-
   ourRequest.send();
+  function renderHTML(data) {
+    var htmlString = "";
+    for(i = 0; i < data.pokemon.length; i++) {
+      htmlString+= "<p>" + data.pokemon[i].name + "</p>"
+    }
+    pokemonContainer.insertAdjacentHTML('beforeend', htmlString)
 
+  }
 
-});
+// });
