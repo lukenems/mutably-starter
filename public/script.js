b6a432cc80dd24e6e11fe014778422e11311d619
Luke Nemy <admin@Admins-MacBook-Pro-10.local>
2017-06-06 10:16:54 -0700

    Updated the script.js file and we console.log'd our JSON in the browser

--- public/script.js
+++ public/script.js
@@ -2,6 +2,17 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  // code in here
+  var ourRequest = new XMLHttpRequest();
+
+  ourRequest.open('GET', 'https://mutably.herokuapp.com/pokemon');
+
+  ourRequest.onload = function() {
+    var pokedexData = JSON.parse(ourRequest.responseText);
+    console.log(pokedexData);
+    // renderHTML(pokedexData);
+  };
+
+  ourRequest.send();
+
 
 });
