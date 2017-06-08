5e8ad0657860c3ac43aae89777c62f766a2a6dfd
smithrm941 <smithrm941@gmail.com>
2017-06-08 12:43:59 -0700

    WIP making buttons work

--- public/script.js
+++ public/script.js
@@ -1,20 +1,33 @@
 console.log("Sanity Check: JS is working!");
-    var pokemonContainer = document.getElementById("pokedex")
-    $('#get-button').on('click', function() {
-      console.log("hooray jquery")
-        $.ajax({
-            method: 'GET',
-            url: 'https://mutably.herokuapp.com/pokemon'
-        }).done(function(data){
-          var htmlString = "";
-          for(i = 0; i < data.pokemon.length; i++) {
-            var pokemonImages = "";
-            imageURL = data.pokemon[i].image
+var pokemonContainer = document.getElementById("pokedex")
 
-            pokemonImages+= "<img src=" + imageURL + ">"
+$('#get-button').on('click', function() {
+  console.log("hooray jquery")
+    $.ajax({
+        method: 'GET',
+        url: 'https://mutably.herokuapp.com/pokemon'
+    }).done(function(data){
+      var htmlString = "";
+      for(i = 0; i < data.pokemon.length; i++) {
+        var pokemonImages = "";
+        imageURL = data.pokemon[i].image
 
-            htmlString+= "<p>" + "<strong>Name: </strong>" +data.pokemon[i].name + " " + "<strong>Pokédex Number: </strong>" + data.pokemon[i].pokedex + " " + "<strong>Evolves from: </strong>" + data.pokemon[i].evolves_from + " " + "</p>" + pokemonImages + "<button>Edit</button>"
-          }
-          pokemonContainer.insertAdjacentHTML('beforeend', htmlString)
-        })
-    });
+        pokemonImages+= "<img src=" + imageURL + ">"
+
+        htmlString+= "<p class='pokemon-info'>" + "<strong>Name: </strong>" +data.pokemon[i].name + " " + "<strong>Pokédex Number: </strong>" + data.pokemon[i].pokedex + " " + "<strong>Evolves from: </strong>" + data.pokemon[i].evolves_from + " " + "</p>" + pokemonImages + "<input type='button' value='Edit' class='edit-button'></input>" + "<input type='button' value='Save' class='save-button' hidden></input>"
+      }
+      pokemonContainer.insertAdjacentHTML('beforeend', htmlString)
+
+      $('.edit-button').on('click', function() {
+        console.log('please work.')
+        $(this).hide();
+        $('.save-button').show();
+        // $(this).children('.pokemon-info').hide();
+        // var thisPokemonInfo = $(this).attr('.pokemon-info')
+        // $(thisPokemonInfo).hide();
+
+        // $("#editTitle").show();
+        // $("#saveTitleButton").show();
+      });
+    })
+});
