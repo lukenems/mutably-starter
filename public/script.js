2ff22852d9965db86ad1df0024036fdb3570b928
Luke Nemy <admin@Admins-MacBook-Pro-10.local>
2017-06-08 16:59:08 -0700

    Added individual Id from JSON to each pokemon <p> element

--- public/script.js
+++ public/script.js
@@ -1,47 +1,63 @@
 console.log("Sanity Check: JS is working!");
 var pokemonContainer = document.getElementById("pokedex")
 
-$("#add-button").click(function() {
-  $(".new-pokemon").show();
-  $("#add-button").hide();
-});
+$(document).ready(function(){
 
-$('#get-button').on('click', function() {
-    $.ajax({
-        method: 'GET',
-        url: 'https://mutably.herokuapp.com/pokemon'
-    }).done(function(data){
-      var htmlString = "";
+    $("#add-button").click(function() {
+      $(".new-pokemon").show();
+      $("#add-button").hide();
+    });
 
-      for(i = 0; i < data.pokemon.length; i++) {
-        var pokemonImages = "";
-        var inputString = "";
-        imageURL = data.pokemon[i].image
+    $('#get-button').on('click', function(event) {
+      event.preventDefault()
+        $.ajax({
+            method: 'GET',
+            url: 'https://mutably.herokuapp.com/pokemon'
+        }).done(function(data){
+          var htmlString = "";
 
-        pokemonImages+= "<img src=" + imageURL + ">"
+          for(i = 0; i < data.pokemon.length; i++) {
+            var pokemonImages = "";
+            var inputString = "";
+            pokemonId = data.pokemon[i]._id;
+            imageURL = data.pokemon[i].image
 
-        inputString += "<form class='edit-info' hidden><input class='edit-info' value=" + data.pokemon[i].name +  "></input>" + "<input class='edit-info' value=" + data.pokemon[i].pokedex + "></input>" + "<input class='edit-info' value=" + data.pokemon[i].evolves_from + "></input></form>"
-
-        htmlString+= "<p class='pokemon-info'>" + "<strong>Name: </strong>" +data.pokemon[i].name + " " + "<strong>Pokédex Number: </strong>" + data.pokemon[i].pokedex + " " + "<strong>Evolves from: </strong>" + data.pokemon[i].evolves_from + " " + "</p>" + pokemonImages + "<input type='button' value='Edit' class='edit-button'></input>" + "<input type='button' value='Save' class='save-button' hidden></input>" + inputString
-      }
-      pokemonContainer.insertAdjacentHTML('beforeend', htmlString)
+            pokemonImages+= "<img src=" + imageURL + ">"
 
 
-      $('.edit-button').on('click', function() {
-        //want fields to pop up to edit pokemon information
-        //working:
-        $(this).hide();
-        $(this).next('.save-button').show();
+            inputString += "<p hidden>" + pokemonId + "</p>" + "<form class='edit-info' hidden><input class='edit-info' value=" + data.pokemon[i].name +  "></input>" + "<input class='edit-info' value=" + data.pokemon[i].pokedex + "></input>" + "<input class='edit-info' value=" + data.pokemon[i].evolves_from + "></input>" + "<input type='submit' value='Save' class='save-button'" + "></input></form>"
 
-        //this is the p element of info hiding:
-        // $(this).prevAll('.pokemon-info').hide();
-        //how about the p element toggle class to edit-info?
+            htmlString+= "<p class='pokemon-info'>" + "<strong>Name: </strong>" +data.pokemon[i].name + " " + "<strong>Pokédex Number: </strong>" + data.pokemon[i].pokedex + " " + "<strong>Evolves from: </strong>" + data.pokemon[i].evolves_from + " " + "</p>" + pokemonImages + "<input type='button' value='Edit' class='edit-button'></input>" + inputString
+          }
+          pokemonContainer.insertAdjacentHTML('beforeend', htmlString)
 
-        //temporarily commented out until we can figure out what's going on:
-        //want fields to pop up where plain text is:
-        // $(this).prevAll('.pokemon-info').hide()
 
-        $(this).nextAll('.edit-info').show();
+          $('.edit-button').on('click', function() {
+            //want fields to pop up to edit pokemon information
+            //working:
+            $(this).hide();
+            $(this).next('.save-button').show();
+
+            //this is the p element of info hiding:
+            // $(this).prevAll('.pokemon-info').hide();
+            //how about the p element toggle class to edit-info?
+
+            //temporarily commented out until we can figure out what's going on:
+            //want fields to pop up where plain text is:
+            // $(this).prevAll('.pokemon-info').hide()
+
+            $(this).nextAll('.edit-info').show();
+          });
+        })
+    });
+
+
+    $(document).on('click', '.save-button', function() {;
+      console.log(pokemonId);
+      var updatedInfo = $('')
+        $.ajax({
+            method: 'PUT',
+            url: 'https://mutably.herokuapp.com/pokemon'+ id,
       });
-    })
+});
 });
